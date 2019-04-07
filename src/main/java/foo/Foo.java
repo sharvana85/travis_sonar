05ab0365fafb2e03f31086a624bfb01e7a0b873b
sharvana85 <Sharvana@gmail.com>
2019-04-07 12:02:43 +0530

    Update Foo.java

--- src/main/java/foo/Foo.java
+++ src/main/java/foo/Foo.java
@@ -7,6 +7,7 @@ public class Foo {
 
     public static int div(int a, int b) throws Exception {
     	if (b == 0) {
+            System.out.println("In div method");
     		throw new UnsupportedOperationException("Can't divide by zero!");
     	}
         return a / b;  
